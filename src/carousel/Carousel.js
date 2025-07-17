e7722450ebba0f5142b90d9d0041d42ff297c515
Dmitriy0212 <demarr68@gmail.com>
2025-07-17 18:01:28 +0300

    fix

--- src/carousel/Carousel.js
+++ src/carousel/Carousel.js
@@ -6,38 +6,65 @@ import { FaChevronRight, FaChevronLeft } from 'react-icons/fa';
 export const Carousel = ({ children }) => {
     const [offSet, setOffSet] = useState(0)
     const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-    const handlLeft = () => {
 
-        setOffSet((currentOffSet) => {
-            const newOffSet = currentOffSet + screenWidth
-            return Math.min(newOffSet, 0)
-        })
-    }
-    /*const handlRight = () => {
-        setOffSet((currentOffSet) => {
-            const newOffSet = currentOffSet - screenWidth
-            return Math.max(newOffSet, -(screenWidth * 6))
-        })
-    }*/
-    useEffect(() => {
+
+    const windowInnerWidth = useCallback(() => {
         const changeWidth = () => {
             setScreenWidth(window.innerWidth);
         };
 
         window.addEventListener('resize', changeWidth);
-
         return () => {
             window.removeEventListener('resize', changeWidth);
         };
     }, []);
 
+    useEffect(() => {
+        windowInnerWidth()
+    }, [windowInnerWidth]);
+
+
+
+    const handlLeft = useCallback(() => {
+        setOffSet((currentOffSet) => {
+            windowInnerWidth()
+            const wind = Math.min(currentOffSet / screenWidth)
+            console.log(-(Math.floor(wind)))
+            if (wind <= 0) {
+                currentOffSet = wind * screenWidth
+                const newOffSet = (Math.floor(wind) * screenWidth + screenWidth)
+                return Math.min(newOffSet, 0)
+            }
+            if (wind === 0) {
+                currentOffSet = wind * screenWidth
+                const newOffSet = currentOffSet + screenWidth
+                return Math.min(newOffSet, +(screenWidth * 6))
+            }
+
+            /*const newOffSet = currentOffSet + screenWidth
+            return Math.min(newOffSet, 0)*/
+        })
+    }, [screenWidth, windowInnerWidth]);
+
 
     const handlRight = useCallback(() => {
         setOffSet((currentOffSet) => {
-            const newOffSet = currentOffSet - screenWidth
-            return Math.max(newOffSet, -(screenWidth * 6))
+            windowInnerWidth()
+            const wind = Math.min(currentOffSet / screenWidth)
+            console.log(Math.floor(wind))
+            if (wind < 0) {
+                currentOffSet = wind * screenWidth
+                const newOffSet = (Math.floor(wind) * screenWidth - screenWidth)
+                return Math.max(newOffSet, -(screenWidth * 6))
+            }
+            if (wind === 0) {
+                currentOffSet = wind * screenWidth
+                const newOffSet = currentOffSet - screenWidth
+                return Math.max(newOffSet, -(screenWidth * 6))
+            }
+
         })
-    }, [screenWidth]);
+    }, [screenWidth, windowInnerWidth]);
 
 
 
@@ -61,20 +88,20 @@ export const Carousel = ({ children }) => {
 
 
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            if (offSet === -(screenWidth * 6)) {
-                setOffSet(0)
-                console.log(offSet)
-            }
-            handlRight()
-        }, 5000);
-
-        return () => {
-            clearTimeout(timer);
-        }
-
-    }, [handlRight, offSet, screenWidth]);
+    /* useEffect(() => {
+         const timer = setTimeout(() => {
+             if (offSet === -(screenWidth * 6)) {
+                 setOffSet(0)
+                 console.log(offSet)
+             }
+             handlRight()
+         }, 5000);
+ 
+         return () => {
+             clearTimeout(timer);
+         }
+ 
+     }, [handlRight, offSet, screenWidth]);*/
 
 
     return (
